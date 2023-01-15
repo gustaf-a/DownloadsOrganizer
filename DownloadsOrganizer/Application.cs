3551b4804a0a1ca67e986238ebed6bb08ed63221
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-01-15 18:18:23 +0100

    Add hosting for dependency injection

---
+++ DownloadsOrganizer/Application.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace DownloadsOrganizer;
+
+public class Application : IHostedService
+{
+    public Application()
+    {
+
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        return;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
--- DownloadsOrganizer/Program.cs
+++ DownloadsOrganizer/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace DownloadsOrganizer
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace DownloadsOrganizer;
+
+public class Program
 {
-    internal class Program
+    public static async void Main(string[] args)
     {
-        static void Main(string[] args)
+
+        Log.Logger = new LoggerConfiguration()
+                        .MinimumLevel.Information()
+                        .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+                        .CreateLogger();
+
+        var builder = Host.CreateDefaultBuilder(args);
+
+        builder.ConfigureServices(
+            services =>
+            {
+                services.AddHostedService<Application>();
+                ConfigureServices(services);
+            }
+            );
+
+        var host = builder.Build();
+
+        try
         {
-            Console.WriteLine("Hello, World!");
+            await host.RunAsync();
         }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "Unexpected error when running application.");
+            throw;
+        }
+    }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddTransient<Application>();
+
+
     }
 }
