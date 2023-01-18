c079fbdb549d609d58993e18c08855609757841d
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-01-18 17:02:22 +0100

    Change ApplicationOptions SourceFolder into array to allow multiple source folders

--- DownloadsOrganizer/Configuration/ApplicationOptions.cs
+++ DownloadsOrganizer/Configuration/ApplicationOptions.cs
@@ -4,7 +4,7 @@ public class ApplicationOptions
 {
     public const string Application = "Application";
 
-    public string SourceFolder { get; set; } = string.Empty;
+    public string[] SourceFolder { get; set; } = Array.Empty<string>();
     public string OutputFolder { get; set; } = string.Empty;
 
     public string CategoryFolderPrefix { get; set; } = "_";
